a26706810f8a4209a0a1cf783f2a7978b42a7d1e
André Felipe C. Leite <andrefelipecl@outlook.com>
2020-10-12 16:23:11 -0300

    Create README.md

---
+++ src/main/java/br/com/afcl/recipeapp/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package br.com.afcl.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author André Felipe C. Leite
+ * @version 1.0
+ * @since 12/10/2020 - 16:38
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "index"})
+	public String getIndexPage(){
+		return "index";
+	}
+
+}
