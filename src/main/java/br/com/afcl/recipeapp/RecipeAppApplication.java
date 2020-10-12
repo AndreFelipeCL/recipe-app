2ff6796e2ae662b0854d0fc530ee27b7d8221a64
André Felipe C. Leite <andrefelipecl@outlook.com>
2020-10-12 16:17:15 -0300

    Start project

---
+++ src/main/java/br/com/afcl/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.afcl.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppApplication.class, args);
+	}
+
+}
